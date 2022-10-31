509677ef733e7be2f516fd1c15f6256e890bd6a5
Andrii <andriutsa1@ukr.net>
2022-10-31 18:32:23 +0200

    draft

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,43 @@ import static com.javapro.lesson21.ProductType.PENCIL;
 
 import com.javapro.lesson21.NoFindElementException;
 import com.javapro.lesson21.Product;
-
+import java.time.Year;
+import javax.xml.crypto.Data;
 
 
 public class Main {
 
   public static void main(String[] args) {
-
-    Product[] products = {new Product(BOOK, 350), new Product(NOTEBOOK, 300),
-        new Product(PENCIL, 200), new Product(PEN, 120), new Product(BOOK, 280),
-        new Product(PENCIL, 200), new Product(PEN, 120), new Product(PEN, 260),
-        new Product(BOOK, 100),
-        new Product(ERASER, 150)};
-
+    ;
     Product product = new Product();
-    product.addAllProductToList(products);
+
+    product.addAllProductToList(new Product(BOOK, 350));
+    product.addAllProductToList(new Product(NOTEBOOK, 300));
+    product.addAllProductToList(new Product(PENCIL, 200));
+    product.addAllProductToList(new Product(PEN, 120));
+    product.addAllProductToList(new Product(BOOK, 280));
+    product.addAllProductToList(new Product(PENCIL, 200));
+    product.addAllProductToList(new Product(PEN, 140));
+    product.addAllProductToList(new Product(PEN, 260));
+    product.addAllProductToList(new Product(BOOK, 100));
+    product.addAllProductToList(new Product(ERASER, 150));
+    product.addAllProductToList(new Product(BOOK, 40, 10));
+    product.addAllProductToList(new Product(BOOK, 60, 15));
+    product.addAllProductToList(new Product(BOOK,70,10,111));
+
+
+
     System.out.println(product.addListWithPrice(250, BOOK));
-    System.out.println(product.addListWithDiscount(10,BOOK));
+    System.out.println(product.addListWithDiscount(10, BOOK));
     try {
-      System.out.println(product.minValue(BOOK));
-    }catch (NoFindElementException e){
+      System.out.println(product.minValue(PENCIL));
+    } catch (
+        NoFindElementException e) {
       System.out.println(e.getMessage());
     }
     System.out.println(product.getProductList());
+    System.out.println(product.addListLastThee());
+    System.out.println(product.addListInThisYear(Year.now(),75,BOOK));
+    System.out.println(product.addMap());
   }
 }
--- src/com/javapro/lesson21/Product.java
+++ src/com/javapro/lesson21/Product.java
@@ -1,22 +1,23 @@
 package com.javapro.lesson21;
 
 
+import java.time.LocalDate;
+import java.time.Year;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Product {
 
   ProductType type;
   private double price;
-  private  List<Product> productList = new ArrayList<>();
+  private double discount;
+  private int id;
+  private final List<Product> productList = new ArrayList<>();
+  private final LocalDate localDate = java.time.LocalDate.now();
 
-
-  public List<Product> getProductList() {
-    return productList;
-  }
 
   public Product(ProductType type, double price) {
     this.type = type;
@@ -24,40 +25,71 @@ public class Product {
 
   }
 
+  public Product(ProductType type, double price, double discount) {
+    this.type = type;
+    this.price = price;
+    this.discount = discount;
+
+  }
+
+  public Product(ProductType type, double price,double discount , int id) {
+    this.type = type;
+    this.price = price;
+    this.discount = discount;
+    this.id = id;
+  }
+
+
   public Product() {
   }
 
-  public void addAllProductToList(Product[] products) {
-    Collections.addAll(productList, products);
+  public void addAllProductToList(Product product) {
+    productList.add(product);
   }
 
   public List<Product> addListWithPrice(double price, ProductType type) {
     return productList.stream()
-        .filter(product -> product.getPrice() > price & product.getType().equals(type)).collect(
-            Collectors.toList());
+        .filter(product -> product.getPrice() > price & product.getType().equals(type))
+        .collect(Collectors.toList());
 
 
   }
 
   public List<Product> addListWithDiscount(double discount, ProductType type) {
     return productList.stream()
-        .filter(product -> product.getType().equals(type))
-        .peek(product -> product.setPrice(product.getPrice() - product.getPrice() / discount)).collect(
-            Collectors.toList());
-
+        .filter(product -> product.getType().equals(type) && product.getDiscount() != 0)
+        .peek(product -> product.setPrice(discount))
+        .collect(Collectors.toList());
 
 
   }
 
-  public Product minValue( ProductType type) {
+  public Product minValue(ProductType type) {
     return productList.stream()
-        .filter(product -> product.getType().equals(type))
+        .filter(product -> product.getType().equals(type) && product.getDiscount() != 0)
         .min(Comparator.comparing(Product::getPrice))
-        .orElseThrow(()->new NoFindElementException("Продукт "+type+" Не найден"));
+        .orElseThrow(() -> new NoFindElementException("Продукт " + type + " Не найден"));
 
   }
 
+  public List<Product> addListLastThee() {
+    return productList.stream().skip(productList.size()-3)
+        .collect(Collectors.toList());
 
+
+  }
+  public Double addListInThisYear(Year year,double maxPrice,ProductType type) {
+
+    return productList.stream()
+        .filter(product -> product.getType().equals(type) && product.getPrice() <= maxPrice
+            && product.getLocalDateTime().getYear() == year.getValue())
+        .mapToDouble(Product::getPrice).sum();
+
+  }
+public Map<ProductType,List<Product>> addMap(){
+  return productList.stream().collect(Collectors.groupingBy(Product::getType));
+
+  }
   public ProductType getType() {
     return type;
   }
@@ -66,15 +98,35 @@ public class Product {
     return price;
   }
 
-  public void setPrice(double price) {
-    this.price = price;
+  public int getId() {
+    return id;
   }
+
+  public LocalDate getLocalDateTime() {
+    return localDate;
+  }
+
+  public double getDiscount() {
+    return discount;
+  }
+
+  public List<Product> getProductList() {
+    return productList;
+  }
+
+  public void setPrice(double discount) {
+    this.price = getPrice() - getPrice() / discount;
+  }
+
 
   @Override
   public String toString() {
     return "Product{" +
         "type=" + type +
         ", price=" + price +
-        '}';
+        ", id=" + id +
+        ", date=" + localDate +
+        ", discount=" + discount +
+       " }\n";
   }
 }
